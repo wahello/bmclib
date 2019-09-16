e5ec8e3562837a3b0b2e5bb2d555b6bb202d990b
Juliano Martinez <juliano.martinez@booking.com>
2019-09-16 16:03:48 +0200

    start actions for redfish

--- providers/redfish/actions.go
+++ providers/redfish/actions.go
@@ -1,10 +1,30 @@
 package redfish
 
+import "github.com/stmcginnis/gofish/redfish"
+
 // PowerCycle reboots the machine via bmc
 func (r *Redfish) PowerCycle() (status bool, err error) {
 	err = r.httpLogin()
 	if err != nil {
 		return status, err
+	}
+
+	entries, err := r.service.Systems()
+	if err != nil {
+		return status, err
+	}
+
+	if len(entries) < 1 {
+		return status, ErrNoSystemEntry
+	}
+
+	for _, e := range entries {
+		if e.SystemType == "Physical" {
+			err = e.Reset(redfish.ForceRestartResetType)
+			if err != nil {
+				panic(err)
+			}
+		}
 	}
 
 	return status, err
